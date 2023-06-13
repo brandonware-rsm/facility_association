4bb05bb5e638d0ec736ae6e1a22946b974f87759
Brandon Ware <brandon.ware@rsmus.com>
2023-06-13 15:00:19 -0400

    adding kv and dl classes

---
+++ datalakecontainer.py
@@ -0,0 +1,55 @@
+from azure.storage.blob import BlobClient, BlobServiceClient
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+import logging
+
+class DataLakeContainer:
+
+    def __init__(self, storage_account_name, connection_string, container_name):
+        
+        self.storage_account_name = storage_account_name
+        self.connection_string = connection_string
+        self.container_name = container_name
+        self.blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        self.container_client = self.blob_service_client.get_container_client(container_name)
+        #self.logger = logging.getLogger('func_get_quickbooks_object.datalakecontainer')
+
+    def upload_file_to_data_lake(self,blob_name,data):
+
+        blob = BlobClient.from_connection_string(conn_str=self.connection_string, container_name=self.container_name,blob_name=blob_name)
+        response = blob.upload_blob(data,overwrite=True)
+        #self.logger.debug(f'{response}')
+        
+        return response
+
+    def read_file_from_data_lake(self,blob_name):
+
+        blob = BlobClient.from_connection_string(conn_str=self.connection_string, container_name=self.container_name,blob_name=blob_name)
+        blob_download = blob.download_blob()
+        data = blob_download.readall()
+
+        return data
+
+    def list_blobs_in_directory(self,directory_path):
+
+        blob_service_client = BlobServiceClient.from_connection_string(conn_str=self.connection_string)
+        container_client = blob_service_client.get_container_client(self.container_name)
+        blobs = container_client.list_blobs(name_starts_with=directory_path)
+        blob_names = [blob.name for blob in blobs]
+
+        return blob_names
+
+
+    def delete_blob_from_data_lake(self,blob_name):
+
+        blob = BlobClient.from_connection_string(conn_str=self.connection_string, container_name=self.container_name,blob_name=blob_name)
+        blob.delete_blob()
+
+
+    def move_blob_from_source_to_destination(self, source_blob_name, destination_blob_name):
+
+        source_blob_client = BlobClient.from_connection_string(conn_str=self.connection_string, container_name=self.container_name,blob_name=source_blob_name)
+        destination_blob_client = BlobClient.from_connection_string(conn_str=self.connection_string, container_name=self.container_name,blob_name=destination_blob_name)
+
+        destination_blob_client.start_copy_from_url(source_blob_client.url)
+        source_blob_client.delete_blob()
---
+++ keyvault.py
@@ -0,0 +1,40 @@
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+import logging
+
+class KeyVault(object):
+
+    def __init__(self,vault_name):
+        self.vault_name = vault_name
+        self.KVUri = 'https://'+ vault_name + '.vault.azure.net/'  
+        self.credential = DefaultAzureCredential(additionally_allowed_tenants=['*'])
+        self.client = SecretClient(vault_url=self.KVUri, credential=self.credential)
+        self.logger = logging.getLogger('func_get_quickbooks_object.keyvault')
+
+    def get_secret(self, secret_name):
+
+        try:
+            secret = self.client.get_secret(secret_name)
+            return secret.value
+        except Exception as e:
+            raise Exception(f"Failed to retrieve secret '{secret_name}' from Azure Key Vault: {e}")
+
+    def set_secret(self, secret_name, secret_value):
+        try:
+            current_secret = self.client.get_secret(secret_name)
+            if current_secret.value == secret_value:
+                self.logger.debug('Secret matches what is already in key vault. No updated required')
+            else:
+                self.client.set_secret(secret_name, secret_value)
+                self.logger.debug('Secret has been successfully updated in Key Vault')
+        except Exception as e:
+            raise Exception(f"Failed to set secret '{secret_name}' in Azure Key Vault: {e}")
+
+    def list_all_secrets(self):
+        secrets = self.client.list_properties_of_secrets()
+
+        return secrets
+
+
+# my_vault = KeyVault('akv-apipoc-dev')
+# print(my_vault.get_secret('mysecret'))
