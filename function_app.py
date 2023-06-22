2103254bf374f0b6eb5b8424d91f78767af40046
Brandon Ware <brandon.ware@rsmus.com>
2023-06-22 16:11:45 -0400

    clean up function app

--- function_app.py
+++ function_app.py
@@ -15,33 +15,26 @@ def blob_trigger(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
-    #print(myblob.name.rsplit('/',1)[-1]) #fa-data-lake-dev/test/raw/landing/PAUTO.D23032.V001.C91691
     #Setting mapping sheet
     mapping_blob = 'test/mapping/fa_premium_record_layout.json'
     # Getting mapping sheet and convert from string to a list
     storage_account_name = 'dlsfadwhdev'
     my_vault = KeyVault('kv-fa-dwh-dev')
     connection_string = my_vault.get_secret('dl-connection-string')
-    # print(my_vault.get_secret('dl-connection-string'))    container_name = 'fa-data-lake-dev'
+    container_name = 'fa-data-lake-dev'
     my_data_lake = DataLakeContainer(storage_account_name, connection_string, container_name)
-    # print(my_data_lake.storage_account_name)
     premium_mapping = my_data_lake.read_file_from_data_lake(mapping_blob).decode('utf-8')
     premium_mapping = json.loads(premium_mapping)
-    # print(type(premium_mapping))
-    # print(premium_mapping)
 
     # Getting test raw file
-    file_name = myblob.name.rsplit('/',1)[-1]#'PAUTO.D23032.V001.C91691'#'PAUTO.D23032.V001.C91691'#'TAUTO.D23054.V300.C90693'#'PAUTO.D23060.V001.C91192'
+    file_name = myblob.name.rsplit('/',1)[-1]
     container_name = 'tempuipdata'
     my_data_lake_raw_file = DataLakeContainer(storage_account_name, connection_string, container_name)
-    # print(my_data_lake.storage_account_name)
     test_file = my_data_lake_raw_file.read_file_from_data_lake(file_name)
-    #print(test_file)
 
     #Cleanse file into list
     test1 = FileCleanse(test_file)
     cleaned_data = test1.decode_file()
-    #print(cleaned_data)
 
     #Delimit cleaned file
     premium_delimit_test1 = FileDelimit(premium_mapping,cleaned_data)
@@ -49,21 +42,14 @@ def blob_trigger(myblob: func.InputStream):
     df = premium_delimit_test1.generate_dataframe(master_list)
     csv_data = premium_delimit_test1.generate_csv_data(df)
 
-    #push csv data into storage account
-    # Getting mapping sheet
-    # storage_account_name = 'dlsfadwhdev'
-    # connection_string = 'DefaultEndpointsProtocol=https;AccountName=dlsfadwhdev;AccountKey=ZJ+p2fUqWsMa0c4Y3+6B9b2efwDclOS+ju4gLNfUsArKb+9bRJWrNudDoibteFrwDcCp5IzpH2Wi+ASt5wfwpA==;EndpointSuffix=core.windows.net'
+    #Push csv data into storage account with UIP
     container_name = 'fa-data-lake-dev'
     data_lake_landing = DataLakeContainer(storage_account_name, connection_string, container_name)
-    # print(my_data_lake.storage_account_name)
     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
     data_lake_landing.upload_file_to_data_lake(f'test/raw/with_pii/{file_name}_{timestamp}.csv',csv_data)
 
-    #Take out (UIP) columns
+    #Take out (UIP) columns and push into different directory
     filtered_columns = [col for col in df.columns if '(UIP)' not in col]
-    #print(filtered_columns)
-    #print(df.columns)
     no_pii_df = df[filtered_columns]
-    #print(no_pii_df)
     csv_data_no_pii = premium_delimit_test1.generate_csv_data(no_pii_df)
     data_lake_landing.upload_file_to_data_lake(f'test/raw/without_pii/{file_name}_{timestamp}.csv',csv_data_no_pii)
